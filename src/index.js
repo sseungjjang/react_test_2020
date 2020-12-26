ccca420fa8d50f24ee16e745b87605dc1c6c8b88
joule <joule@naver.com>
2020-12-26 16:19:19 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-var element = React.createElement('hi', {className:'greeting'}, 'Hello world!');
-
-ReactDOM.render(
-element, document.getElementById('root')
-);
-reportWebVitals();
+ReactDOM.render(<App/>, document.getElementById("root"));
